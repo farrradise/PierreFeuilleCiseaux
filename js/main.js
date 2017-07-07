b2113a634d5d84642dad997f899c0745fe784f0b
farrradise <marzini.f@gmail.com>
2017-07-07 16:16:12 +0200

    tried to matched my algo with some dom

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ var etapeName = document.getElementById("askForName");
 var etapePlay = document.getElementById("onJoue");
 var etapeJoue = document.getElementById("play");
 var etapeAnim = document.getElementById("animation");
+var etapeScor = document.getElementById("score");
+
 etapePlay.style.visibility = "hidden";
 etapeJoue.style.visibility = "hidden";
 etapeAnim.style.visibility = "hidden";
-
+etapeScor.style.visibility = "hidden";
 
 document.getElementById("btnAskForName").addEventListener("click", switchToWannaPlay);
 
@@ -14,10 +16,10 @@ document.getElementById("btnAskForName").addEventListener("click", switchToWanna
 function switchToWannaPlay() {
   prenom = document.getElementsByTagName("input")[0].value;
   document.getElementById("lePrenom").innerHTML = prenom;
+  document.getElementById("Joueur1").innerHTML = prenom;
   etapePlay.style.visibility = "visible";
   etapeName.style.visibility = "hidden";
 }
-
 // prompt(prenom);
 
 function switchToTuJoues() {
@@ -27,24 +29,34 @@ function switchToTuJoues() {
   h1Visible = document.getElementsByTagName("h1")[0];
   bubbleVisible.style.visibility = "hidden";
   h1Visible.style.visibility = "hidden";
+  etapeScor.style.visibility = "visible";
 }
 
 
-/*
+var userChoice = document.getElementById("ciseaux").addEventListener("click", lancelejeu);
+var userChoice = document.getElementById("papier").addEventListener("click", lancelejeu);
+var userChoice = document.getElementById("pierre").addEventListener("click", lancelejeu);
 
+
+function lancelejeu() {
     var userPoint = 0 , ordiPoint = 0;
     var userChoice ="", ordiChoice = "";
     var allOpt = ["pierre", "papier", "ciseaux"];
 
-
-    for (i = 0; (userPoint || ordiPoint) !== 3; i++) {
+//    for (i = 0; (userPoint || ordiPoint) !== 3; i++) {
 
     ordiChoice = allOpt[Math.floor(Math.random() * allOpt.length)];
 //    console.log(ordiChoice);
 
-    userChoice = prompt("Ecrivez 'pierre' 'papier' ou 'ciseaux'").toLowerCase();
+//    userChoice = prompt("Ecrivez 'pierre' 'papier' ou 'ciseaux'").toLowerCase();
 //    console.log(userChoice);
-
+    if (userChoice == document.getElementById("ciseaux")) {
+      userChoice = "ciseaux";
+    } else if (userChoice == document.getElementById("papier")) {
+      userChoice = "papier";
+    } else {
+      userChoice = "pierre";
+    }
 
     if ( userChoice == ordiChoice ) {
       alert("égalité");
@@ -89,18 +101,21 @@ function switchToTuJoues() {
 
             break;
 
-            default:
-            alert("on avait dit 'pierre' 'feuille' ou 'ciseaux', ne cherche pas à improviser");
+            // default:
+            // alert("on avait dit 'pierre' 'feuille' ou 'ciseaux', ne cherche pas à improviser");
         }
 
     }
 
-    alert("ordi : "+ ordiPoint + " \nvous : "+ userPoint);
-    }
+    document.getElementById("userPoint").innerHTML = userPoint;
+    document.getElementById("ordiPoint").innerHTML = ordiPoint;
 
-    if (ordiPoint > userPoint) {
+//    }
+
+    if (ordiPoint === 3) {
     alert("T'as perdu contre une machine !!!!");
-  } else {
+  }
+    if (userPoint === 3) {
     alert("Bravo, tu as gagné");
   }
-*/
+}

a111caaf7eb62f60f9ccdd01ed8f4b4a3ffe820a
farrradise <marzini.f@gmail.com>
2017-07-07 12:23:30 +0200

    start style of first part  indeX.html

---
+++ js/main.js
@@ -0,0 +1,80 @@
+var etapeName = document.getElementById("askForName");
+var etapePlay = document.getElementById("play");
+var etapeAnim = document.getElementById("animation");
+etapeName.style.visibility = "hidden";
+etapePlay.style.visibility = "hidden";
+etapeAnim.style.visibility = "hidden";
+/*
+
+    var userPoint = 0 , ordiPoint = 0;
+    var userChoice ="", ordiChoice = "";
+    var allOpt = ["pierre", "papier", "ciseaux"];
+
+
+    for (i = 0; (userPoint || ordiPoint) !== 3; i++) {
+
+    ordiChoice = allOpt[Math.floor(Math.random() * allOpt.length)];
+//    console.log(ordiChoice);
+
+    userChoice = prompt("Ecrivez 'pierre' 'papier' ou 'ciseaux'").toLowerCase();
+//    console.log(userChoice);
+
+
+    if ( userChoice == ordiChoice ) {
+      alert("égalité");
+    //  userChoice = prompt("Ecrivez 'pierre' 'papier' ou 'ciseaux'").toLowerCase();
+    } else {
+        switch (userChoice) {
+
+            case "pierre":
+            alert("l'ordi avait choisi : "+ ordiChoice);
+
+              if (ordiChoice == "ciseaux") {
+                alert("you win !");
+                userPoint++;
+              } else {
+                alert("you loose !");
+                ordiPoint++;
+              }
+            break;
+
+            case "papier":
+            alert("l'ordi avait choisi : "+ ordiChoice);
+
+              if (ordiChoice == "pierre") {
+                alert("you win !");
+                userPoint++;
+              } else {
+                alert("you loose !");
+                ordiPoint++;
+              }
+            break;
+
+            case "ciseaux":
+            alert("l'ordi avait choisi : "+ ordiChoice);
+
+              if (ordiChoice == "papier") {
+                alert("you win !");
+                userPoint++;
+              } else {
+                alert("you loose !");
+                ordiPoint++;
+              }
+
+            break;
+
+            default:
+            alert("on avait dit 'pierre' 'feuille' ou 'ciseaux', ne cherche pas à improviser");
+        }
+
+    }
+
+    alert("ordi : "+ ordiPoint + " \nvous : "+ userPoint);
+    }
+
+    if (ordiPoint > userPoint) {
+    alert("T'as perdu contre une machine !!!!");
+  } else {
+    alert("Bravo, tu as gagné");
+  }
+*/
